95e1739f05c97fd0ec548ac8b113c47dfb9d2445
Sola Jegede <sjegede1@gmail.com>
2023-07-27 15:26:49 -0500

    Create routes in App.js. Next is to build out component wireframe in Main.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import {useEffect,useState,useContext} from 'react';
-import {AppContext} from './contexts/context'
+import {AppContext} from './contexts/context';
+import {Route, Routes} from "react-router-dom"
 
 function App() {
   let {getData} = useContext(AppContext)
@@ -10,7 +11,10 @@ function App() {
   },[])
   return (
     <div className="App">
-      here
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/alerts" element={<Alerts />} />
+      </Routes>
     </div>
   );
 }
