ea22463e3ec5bfec60c111555ca9624eb7b55e74
Sola Jegede <sjegede1@gmail.com>
2023-07-27 18:10:19 -0500

    Edited format to add 0 to single digit numerber e.g. 6AM is 06:00

--- src/components/CurrentTime.jsx
+++ src/components/CurrentTime.jsx
@@ -7,9 +7,12 @@ function CurrentTime() {
     setTimeNow(new Date())
   }
   setTimeout(changeTime,1000)
+  let hh = String(timeNow.getHours()).length == 2 ? String(timeNow.getHours()) : `0${String(timeNow.getHours())}`;
+  let mm = String(timeNow.getMinutes()).length == 2 ? String(timeNow.getMinutes()) : `0${String(timeNow.getMinutes())}`;
+  let ss = String(timeNow.getSeconds()).length == 2 ? String(timeNow.getSeconds()) : `0${String(timeNow.getSeconds())}`;
   return (
     <div className="currrent-time">
-      {timeNow.getHours()}:{timeNow.getMinutes()}:{timeNow.getSeconds()}
+      {hh}:{mm}:{ss}
     </div>
   )
 }
