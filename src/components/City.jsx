45051457be3022c0b4019693e9b506a828f0ae10
Efa Higi <efawakene2005@gmail.com>
2023-07-28 11:39:37 -0500

    second commit

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useContext } from 'react';
+import { AppContext } from '../contexts/context';
 
-
+// export const AppContext = createContext()
 function City() {
+  let {currentCity} = useContext(AppContext)
+
+//   const handleChange = (event) => {
+//     setCity(event.target.value);
+//   };
+//   const handleSubmit = (event) => {
+//     //prevents page from reloading upon form submission
+//     event.preventDefault();
+//     setCurrentCity([...currentCity, data]);
+//     setCity('');
+//   };
+
   return (
-    <div>Dallas</div>
+  <div className='cityName'>
+    {currentCity}
+  </div>
   )
 }
 
--- src/components/DailyForecast.jsx
+++ src/components/DailyForecast.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { useState,useContext } from 'react';
+import { AppContext } from '../contexts/context';
+
 
 function DailyForecast() {
+ let {data} = useContext(AppContext);
+
+ const daysToDisplay = 3;
+ if (!data ){
+  return <></>
+ }
+ const getDailyAr= () => {
+  let daysToDisplay = new Date().getDay();
+  let allDays = [];
+  data.forecast.forecastday.forEach((day) => {
+    allDays.push(day) 
+  });
+
+  return  allDays;
+};  
+
   return (
-    <div>DailyForecast</div>
+    <div className="dailyforecast">
+    <h3>Daily Forecast</h3>
+    <div className='forecastday'>
+      {data.forecast.forecastday.map((day, index) => {
+        return  (
+          <div className="forecast-daily" key={index}>
+            <h5>{day.date} </h5>
+            <img src={day.day.condition.icon} alt=""  />
+            <p>{day.day.maxtemp_f}/{day.day.mintemp_f}</p>
+          </div>
+        )
+      })}
+    </div>
+    </div>
   )
 }
 
-export default DailyForecast
+export default DailyForecast;
