5e29efe08cf22b374500ea3351b0a9b60f618cbf
Hu <huang.h0709@gmail.com>
2023-07-28 09:02:09 -0400

    completed weather display component

--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React,{useContext} from 'react'
+
+import {AppContext} from '../contexts/context'
 
 function WeatherDisplay() {
-  return (
-    <div>WeatherDisplay</div>
-  )
+  let {data} = useContext(AppContext)
+  const loaded = ()=>{
+    return(
+          <div className="wdInfo">
+      <img src={data.current.condition.icon} alt="" className="wdIcon"/>
+      <div className="wdSubInfo">
+        <div className="wdTemperature">{data.current.temp_f}° F</div>
+        <div className="wdCurrentWeather">{data.current.condition.text}</div>
+        <div className="wdHumidity">Humidity: {data.current.humidity}</div>
+        <div className="wdWindSpd">Wind speed: {data.current.wind_mph}</div>
+      </div>
+    </div>
+    )
+  }
+  const loading = ()=>{
+    return(
+      <div>Loading...</div>
+    )
+  }
+  return data ? loaded():loading();
 }
 
 export default WeatherDisplay
