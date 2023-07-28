48b0f4f8340c1fad4cefc1473fb0830731093b8a
Hu <huang.h0709@gmail.com>
2023-07-27 23:20:40 -0400

    still making searchbar

---
+++ src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CitySearchBar from '../components/CitySearchBar'
+
+function Main() {
+  return (
+    <div>
+      <CitySearchBar />
+    </div>
+  )
+}
+
+export default Main
