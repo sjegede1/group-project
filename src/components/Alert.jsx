fabcd7a18b82503f47f514b92c922c467ebfdbdc
Hu <huang.h0709@gmail.com>
2023-07-28 15:00:33 -0400

    added sunrise/set and moonrise/set to alert component. also added time and location header to alerts page

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -7,10 +7,22 @@ function Alert() {
     const loaded =()=>{
         return (
             <div>
-                <p className='alertInfo'>Sunrise: {data.forecast.forecastday[0].astro.sunrise}</p>
-                <p className="alertInfo">Sunset: {data.forecast.forecastday[0].astro.sunset}</p>
-                <p className="alertInfo">Moonrise: {data.forecast.forecastday[0].astro.moonrise}</p>
-                <p className="alertInfo">Moonset: {data.forecast.forecastday[0].astro.moonset}</p>
+                <p className='alertInfo'>Sunrise: 
+                <img src="https://cdn-icons-png.flaticon.com/512/8179/8179067.png" alt="" className="alertIcon"/>
+                {data.forecast.forecastday[0].astro.sunrise}</p>
+                <p className="alertInfo">Sunset: 
+                <img src="https://media.istockphoto.com/id/1158093387/vector/isolated-sunset-gradient-on-white-background.jpg?s=612x612&w=0&k=20&c=Iy94KxslPLie6RRIzsRC4zWXXl18SxEQ1Os5E_5TL0A=" alt="" className="alertIcon"/>
+                {data.forecast.forecastday[0].astro.sunset}</p>
+
+                <p className="alertInfo">Moonrise: 
+                    <img src="https://static.thenounproject.com/png/971176-200.png" alt="" className="alertIcon"/>
+                    {data.forecast.forecastday[0].astro.moonrise}
+                </p>
+                <p className="alertInfo">Moonset: 
+                    <img src="https://static.thenounproject.com/png/4669590-200.png" alt="" className="alertIcon"/>
+                {data.forecast.forecastday[0].astro.moonset}</p>
+                <img src="https://griffithobservatory.org/wp-content/uploads/2021/03/moon_phases_uppergraphic-scaled.jpg" alt="" className = "moonphaseImg"/>
+                <p className="alertInfo">Moon Phase: {data.forecast.forecastday[0].astro.moon_phase}</p>
             </div>
           )
     }
