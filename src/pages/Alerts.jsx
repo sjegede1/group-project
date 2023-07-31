515c7cace3929f851a78a469d5eef65367ab32a7
Sola Jegede <sjegede1@gmail.com>
2023-07-30 20:55:18 -0500

    Used actual css not the test files

--- src/pages/Alerts.jsx
+++ src/pages/Alerts.jsx
@@ -3,7 +3,7 @@ import Alert from '../components/Alert'
 import City from '../components/City'
 import CurrentTime from '../components/CurrentTime'
 import Aqi from '../components/Aqi'
-import "./Alerts-test.css"
+import "./Alerts.css"
 import Nav from '../components/Nav'
 
 function Alerts() {
